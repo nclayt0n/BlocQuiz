3d896f618c289fd586767902a877efd825121267
Nicole Clayton <nicoleclayton@Nicoles-MacBook-Pro.local>
2019-06-28 18:02:46 -0400

    added correct answer page

--- datastore.js
+++ datastore.js
@@ -1,63 +1,63 @@
 let STORE = [{
-        Question: 'Where is the humerus bone?',
-        Answers: [
+        question: 'Where is the humerus bone?',
+        answers: [
             'In your lower leg?',
             'In your upper arm?',
             'In your lower arm?',
             'In your upper leg?'
         ],
-        CorrectAnswer: 'The humerous bone is located in your upper arm, it is connected at the glenohumeral joint.',
-        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Humerus_-_anterior_view.png/1200px-Humerus_-_anterior_view.png'
+        correctAnswer: 'The humerous bone is located in your upper arm, it is connected at the glenohumeral joint.',
+        icon: { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Humerus_-_anterior_view.png/1200px-Humerus_-_anterior_view.png', alt: 'humerus bone' }
     },
     {
 
-        Question: 'Where are your metatarsels?',
-        Answers: ['In your hand?',
+        question: 'Where are your metatarsels?',
+        answers: ['In your hand?',
             'In your arm?',
             'In your leg?',
             'In your foot?'
         ],
-        CorrectAnswer: 'Metatarsels are the long bones, located inbetween your ankle and your toes.',
-        icon: 'http://www.orthopaedia.com/media/2018/8/5ff20309-9275-4806-8903-0d10b41a3ecd-1533248194197.png'
+        correctAnswer: 'Metatarsels are the long bones, located inbetween your ankle and your toes.',
+        icon: { src: 'http://www.orthopaedia.com/media/2018/8/5ff20309-9275-4806-8903-0d10b41a3ecd-1533248194197.png', alt: 'metatarsel bones' }
     },
     {
-        Question: 'What is the anatomical name for shoulder blade?',
-        Answers: ['Clavicle',
+        question: 'What is the anatomical name for shoulder blade?',
+        answers: ['Clavicle',
             'Scaphoid',
             'Sternum',
             'Scapula'
         ],
-        CorrectAnswer: 'Scapula is also referred to as the wingbone.',
-        icon: 'https://web.duke.edu/anatomy/siteParts/LWW_atlas_labeled/Plate-2-03B.jpg'
+        correctAnswer: 'Scapula is also referred to as the wingbone.',
+        icon: { src: 'https://web.duke.edu/anatomy/siteParts/LWW_atlas_labeled/Plate-2-03B.jpg', alt: 'scapula bone' }
     },
     {
-        Question: 'Where is your femur?',
-        Answers: ['In your lower leg?',
+        question: 'Where is your femur?',
+        answers: ['In your lower leg?',
             'In your upper arm?',
             'In your lower arm?',
             'In your upper leg?'
         ],
-        CorrectAnswer: 'The femur is located in your upper leg and is one of the strongest bones.',
-        icon: 'http://www.innerbody.com/assets/Femur%20(1).png'
+        correctAnswer: 'The femur is located in your upper leg and is one of the strongest bones.',
+        icon: { src: 'http://www.innerbody.com/assets/Femur%20(1).png', alt: 'femur bone' }
     },
     {
-        Question: 'Where is the IT band located ?',
-        Answers: [
+        question: 'Where is the IT band located ?',
+        answers: [
             'At the Apple Bar',
             'In the lower leg',
             'In the upper leg',
             'In the ankle'
         ],
-        CorrectAnswer: 'The IlioTibial band is located on the side of the upper leg, from the hip down to the knee.',
-        icon: 'https: //squatuniversitydotcom.files.wordpress.com/2017/05/it-band.png?w=870'
+        correctAnswer: 'The IlioTibial band is located on the side of the upper leg, from the hip down to the knee.',
+        icon: { src: 'https://i0.wp.com/news.harvard.edu/wp-content/uploads/2015/08/itband_605.jpg?fit=605%2C403&ssl=1', alt: 'Iliotibial band' }
     }, {
-        Question: 'What are the bones of the spine called ?',
-        Answers: ['Metatarsels',
+        question: 'What are the bones of the spine called ?',
+        answers: ['Metatarsels',
             'Metacarpels',
             'Vertebraes',
             'Vessels'
         ],
-        CorrectAnswer: 'There are 33 Vertebraes in the human body.',
-        icon: 'http: //images.huffingtonpost.com/2013-10-17-AnatBanielMethodSpine.jpg'
-    },
+        correctAnswer: 'There are 33 Vertebraes in the human body.',
+        icon: { src: 'http://www.yogasya.in/Images/Rites/spine.JPG', alt: 'spine vertebraes' }
+    }
 ];
