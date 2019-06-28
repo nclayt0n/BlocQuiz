e8ba3f63d499ab54c202c5fc0ce64d9a8e5a29ae
Nicole Clayton <nicoleclayton@Nicoles-MacBook-Pro.local>
2019-06-27 21:17:36 -0400

    html bones,requirementsdoc, and an array of question objects

---
+++ datastore.js
@@ -0,0 +1,103 @@
+let STORE = [{
+        Question: 'Where is the humerus bone?',
+        Answers: [
+            'In your lower leg?',
+            'In your upper arm?',
+            'In your lower arm?',
+            'In your upper leg?'
+        ],
+        CorrectAnswer: 'The humerous bone is located in your upper arm, it is connected at the glenohumeral joint.',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Humerus_-_anterior_view.png/1200px-Humerus_-_anterior_view.png'
+    },
+    {
+
+        Question: 'Where are your metatarsels?',
+        Answers: ['In your hand?',
+            'In your arm?',
+            'In your leg?',
+            'In your foot?'
+        ],
+        CorrectAnswer: 'Metatarsels are the long bones, located inbetween your ankle and your toes.',
+        icon: 'http://www.orthopaedia.com/media/2018/8/5ff20309-9275-4806-8903-0d10b41a3ecd-1533248194197.png'
+    },
+    {
+        Question: 'What is the anatomical name for shoulder blade?',
+        Answers: ['Clavicle',
+            'Scaphoid',
+            'Sternum',
+            'Scapula'
+        ],
+        CorrectAnswer: 'Scapula is also referred to as the wingbone.',
+        icon: 'https://web.duke.edu/anatomy/siteParts/LWW_atlas_labeled/Plate-2-03B.jpg'
+    },
+    {
+        Question: 'Where is your femur?',
+        Answers: ['In your lower leg?',
+            'In your upper arm?',
+            'In your lower arm?',
+            'In your upper leg?'
+        ],
+        CorrectAnswer: 'The femur is located in your upper leg and is one of the strongest bones.',
+        icon: 'http://www.innerbody.com/assets/Femur%20(1).png'
+    },
+
+    {
+        Question: 'Where is your mandible located ?',
+        Answers: ['In your face ?',
+            'In your chest ?',
+            'In your back ?',
+            'In your foot ?'
+        ],
+        CorrectAnswer: 'The manidble is the lowest bone of the face, also referred to as your jaw.',
+        icon: 'https: //www.innerbody.com/assets/Mandible.png'
+    },
+    {
+        Question: 'What is a fossa ?',
+        Answers: ['Remains of prehistoric organism preserved in rock.',
+            'Shallow depression.',
+            'Collagen',
+            'Fascia'
+        ],
+        CorrectAnswer: 'The cubital fossa is on the opposite the elbow.',
+        icon: 'https: //s3.amazonaws.com/classconnection/487/flashcards/12299487/jpg/fossa-156BB93FE5749337EF4-thumb400.jpg'
+    }, {
+        Question: 'Where is the IT band located ?',
+        Answers: [
+            'At the Apple Bar',
+            'In the lower leg',
+            'In the upper leg',
+            'In the ankle'
+        ],
+        CorrectAnswer: 'The IlioTibial band is located on the side of the upper leg, from the hip down to the knee.',
+        icon: 'https: //squatuniversitydotcom.files.wordpress.com/2017/05/it-band.png?w=870'
+    }, {
+        Question: 'Which is part of the heart ?',
+        Answers: [
+            'Olfactory Area',
+            'Wernickes Area',
+            'Aorta',
+            'Duodenum'
+        ],
+        CorrectAnswer: 'The Aorta is the largest artery in the body.',
+        icon: 'https: //upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Aorta_scheme_noTags.svg/1200px-Aorta_scheme_noTags.svg.png'
+    }, {
+        Question: 'What are the bones of the spine called ?',
+        Answers: ['Metatarsels',
+            'Metacarpels',
+            'Vertebraes',
+            'Vessels'
+        ],
+        CorrectAnswer: 'There are 33 Vertebraes in the human body.',
+        icon: 'http: //images.huffingtonpost.com/2013-10-17-AnatBanielMethodSpine.jpg'
+    }, {
+        Question: "How many days does it take a person 's skin to completely replace itself?",
+        Answers: [
+            '7 days',
+            '14 days',
+            '27 days',
+            '34 days'
+        ],
+        CorrectAnswer: 'The external layer of skin is called the epidermis and replaces itself approximately every 27 days.',
+        icon: 'https: //livelovefruit.com/wp-content/uploads/2015/08/Howbodyrebuilds.jpg'
+    }
+];
