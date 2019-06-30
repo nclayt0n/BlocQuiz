ba9d3426cf9546e8dcca890cfd6605f87e236fad
Nicole Clayton <nicoleclayton@Nicoles-MacBook-Pro.local>
2019-06-29 23:29:59 -0400

    made a homepage then a main page-downsized- got the functionality working w/ js and jquery then finished it up with styles in css

--- datastore.js
+++ datastore.js
@@ -1,55 +1,33 @@
-let STORE = [{
+const STORE = [{
         question: 'Where is the humerus bone?',
-        answers: [
-            'In your lower leg?',
-            'In your upper arm?',
-            'In your lower arm?',
-            'In your upper leg?'
-        ],
-        correctAnswer: 'In your upper arm?',
+        answers: ['In your lower leg', 'In your upper arm', 'In your lower arm', 'In your upper leg'],
+        correctAnswer: 'In your upper arm',
         answerFact: 'The humerous bone is located in your upper arm, it is connected at the glenohumeral joint.',
         icon: { src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Humerus_-_anterior_view.png/1200px-Humerus_-_anterior_view.png', alt: 'humerus bone' }
     },
     {
         question: 'What is the anatomical name for shoulder blade?',
-        answers: ['Clavicle',
-            'Scaphoid',
-            'Sternum',
-            'Scapula'
-        ],
+        answers: ['Clavicle', 'Scaphoid', 'Sternum', 'Scapula'],
         correctAnswer: 'Scapula',
         answerFact: 'Scapula is also referred to as the wingbone.',
         icon: { src: 'https://web.duke.edu/anatomy/siteParts/LWW_atlas_labeled/Plate-2-03B.jpg', alt: 'scapula bone' }
     },
     {
         question: 'Where is your femur?',
-        answers: ['In your lower leg?',
-            'In your upper arm?',
-            'In your lower arm?',
-            'In your upper leg?'
-        ],
-        correctAnswer: 'In your upper leg?',
+        answers: ['In your lower leg', 'In your upper arm', 'In your lower arm', 'In your upper leg'],
+        correctAnswer: 'In your upper leg',
         answerFact: 'The femur is located in your upper leg and is one of the strongest bones.',
         icon: { src: 'http://www.innerbody.com/assets/Femur%20(1).png', alt: 'femur bone' }
     },
     {
         question: 'Where is the IT band located ?',
-        answers: [
-            'At the Apple Bar',
-            'In the lower leg',
-            'In the upper leg',
-            'In the ankle'
-        ],
+        answers: ['At the Apple Bar', 'In the lower leg', 'In the upper leg', 'In the ankle'],
         correctAnswer: 'In the upper leg',
         answerFact: 'The IlioTibial band is located on the side of the upper leg, from the hip down to the knee.',
         icon: { src: 'https://i0.wp.com/news.harvard.edu/wp-content/uploads/2015/08/itband_605.jpg?fit=605%2C403&ssl=1', alt: 'Iliotibial band' }
     }, {
         question: 'What are the bones of the spine called ?',
-        answers: ['Metatarsels',
-            'Metacarpels',
-            'Vertebraes',
-            'Vessels'
-        ],
+        answers: ['Metatarsels', 'Metacarpels', 'Vertebraes', 'Vessels'],
         correctAnswer: 'Vertebraes',
         answerFact: 'There are 33 Vertebraes in the human body.',
         icon: { src: 'http://www.yogasya.in/Images/Rites/spine.JPG', alt: 'spine vertebraes' }
---
+++ questions.js
@@ -0,0 +1,74 @@
+let score = 0;
+let questionNumber = 1;
+let choosenAnswer;
+
+function selectedAnswer() {
+    $('.answerButton').on('click', event => {
+        choosenAnswer = ($(event.target).attr('value'));
+        console.log(choosenAnswer);
+        //need to be able to choose and then do this on submit
+    })
+}
+
+
+function ontoNextQuestion() {
+    $('#nextQuestion').on('click', event => {
+        //$('.answerButton').removeAttr('checked');
+        questionNumber++;
+        // questionNumber = Math.min(questionNumber, STORE.length);
+        // //this is temporary. eventually end of questions go to final score. 
+        if (questionNumber <= STORE.length) {
+            updateCurrentQuestionCount();
+            displayQuestion();
+            $('#QandAForm').show();
+            $('#correctAnswer').hide();
+        } else {
+            $('#finalScore').show();
+            $('#correctAnswer').hide();
+        }
+    })
+}
+
+function submitQuestion() {
+    $('#submitAnswer').on('click', event => {
+        //$('.answerButton').removeAttr('checked');
+        let index = questionNumber - 1;
+        if (choosenAnswer === STORE[index].correctAnswer) {
+            score++;
+        }
+        $('#answerInfo').text(STORE[index].answerFact);
+        $('#answerInfoImg').attr('src', STORE[index].icon.src).attr('alt', STORE[index].icon.alt).addClass('factImg');
+        updateCurrentQuestionCount();
+        $('#QandAForm').hide();
+        $('#correctAnswer').show();
+    })
+}
+
+function displayQuestion() {
+    $('.answerButton').each((index, element) => { element.checked = false; });
+    let index = questionNumber - 1;
+    $('#question').text(STORE[index].question);
+    for (let i = 0; i < 4; i++) {
+        $(`#option${i}`).val(STORE[index].answers[i]);
+    }
+    for (let i = 0; i < 4; i++) {
+        $(`#selected${i}`).text(STORE[index].answers[i]);
+    }
+}
+
+function updateCurrentQuestionCount() {
+    $('#currentQuestion').text(questionNumber);
+    $('#currentScore').text(score);
+    $('#finalScore').html(`<div id="finalScoreCount">Hope you enjoyed this Anatomy Quiz, and learned a lot!!<br>You scored ${score} points.<br> Click the skeleton to begin again.</div>`);
+    $('#totalQuestions').text(STORE.length);
+}
+
+function createQuiz() {
+    updateCurrentQuestionCount();
+    displayQuestion();
+    submitQuestion();
+    ontoNextQuestion();
+    selectedAnswer();
+    finalScoreHtml()
+}
+$(createQuiz);
